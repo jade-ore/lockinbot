181d29ee52f8fa21ec4653c3f815386e808e2699
jade_ore <jaydenthejayden1009@gmail.com>
2025-08-14 17:25:19 -0500

    try again

--- webserver.py
+++ webserver.py
@@ -7,7 +7,7 @@ def home():
     return "Discord bot is hereee"
 
 def run():
-    app.run(host="0.0.0.0", port=3456)
+    app.run(host="0.0.0.0", port=3609)
 
 def keep_alive():
     t = Thread(target=run)
