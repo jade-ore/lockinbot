9605936c0e9457f6f0d0fe6399cda345800630cf
jade_ore <jaydenthejayden1009@gmail.com>
2025-08-14 17:15:58 -0500

    changed port

--- webserver.py
+++ webserver.py
@@ -7,7 +7,7 @@ def home():
     return "Discord bot is hereee"
 
 def run():
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=3456)
 
 def keep_alive():
     t = Thread(target=run)
